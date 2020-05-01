887642f6bb90f8950a6a57e29b44dc94b497b1d2
Stéphane Wong <stephane.wong-ext@seloger.com>
2020-05-01 19:15:59 +0200

    Update MySQLClientBuilder

--- src/main/scala/com/scalaws/models/dbs/connexion/MySQLClientBuilder.scala
+++ src/main/scala/com/scalaws/models/dbs/connexion/MySQLClientBuilder.scala
@@ -1,7 +1,7 @@
 package com.scalaws.models.dbs.connexion
 
 import com.mysql.cj.jdbc.MysqlDataSource
-import com.scalaws.configs.dbs.PostgreSQLConfigBuilder
+import com.scalaws.configs.dbs.{MySQLConfigBuilder, PostgreSQLConfigBuilder}
 import com.typesafe.config.Config
 import com.zaxxer.hikari.{HikariConfig, HikariDataSource}
 import io.getquill.{MySQLDialect, MysqlJdbcContext, SnakeCase}
@@ -9,17 +9,18 @@ import io.getquill.{MySQLDialect, MysqlJdbcContext, SnakeCase}
 case class MySQLClientBuilder(config: Config)
   extends DatabaseClientBuilder[MySQLDialect, SnakeCase, MysqlJdbcContext[SnakeCase]](config) {
 
-  protected val myqbuilder = PostgreSQLConfigBuilder(config)
+  override val sqlBuilder = MySQLConfigBuilder(config)
+  import sqlBuilder._
 
   override val url: String =
-    s"jdbc:postgresql://${myqbuilder.host}:${myqbuilder.port.getOrElse(5432)}/${myqbuilder.db}"
+    s"jdbc:mysql://address=(host=$host)(port=$port)(user=$user)(password=$pwd)/$db"
 
   override def getConnection = {
     val myqDataSource = new MysqlDataSource()
     val cfg = new HikariConfig()
     myqDataSource.setUrl(url)
     cfg.setDataSource(myqDataSource)
-    new MysqlJdbcContext(SnakeCase,new HikariDataSource(cfg))
+    new MysqlJdbcContext(SnakeCase, new HikariDataSource(cfg))
   }
 
 }
