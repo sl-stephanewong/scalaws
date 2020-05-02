cbb9c4d70f179c25673250bec3fbf19e7e20d81c
Stéphane Wong <stephane.wong-ext@seloger.com>
2020-05-02 23:14:44 +0200

    Add rds type enum

--- src/main/scala/com/scalaws/models/Model.scala
+++ src/main/scala/com/scalaws/models/Model.scala
@@ -1,3 +1,11 @@
 package com.scalaws.models
 
+object RDSType extends Enumeration {
+  type RDSType = Value
+  val H2 = Value("H2")
+  val Mysql = Value("Mysql")
+  val Postgres = Value("Postgres")
+  val SqlServer = Value("SqlServer")
+}
+
 trait Record extends Product with Serializable
